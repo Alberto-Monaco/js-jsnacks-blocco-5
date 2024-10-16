7d1b5c4e2265b2275897feabdf34481d35c2e5d5
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-16 13:28:22 +0200

    complete 1-2 snack

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,10 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
-];
+	{ name: 'Paolo', age: 35 },
+	{ name: 'Giulia', age: 24 },
+	{ name: 'Marco', age: 67 }
+]
+
+console.log(people.map((NamePerson) => NamePerson.name))
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'

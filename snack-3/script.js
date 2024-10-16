e00852bd97191b74d7f3ef1547f648fc2cc338c6
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-16 13:39:46 +0200

    complete 4 snack

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,3 +3,4 @@ const numbers = [2, 8, 4, 7, 2, 87]
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
 const NumberPlusOne = numbers.map((number) => number + 1)
+console.log(NumberPlusOne)
